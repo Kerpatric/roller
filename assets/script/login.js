9c78bd7c35ed7bb56609f14d8952a9452883cc6e
Eric M <ericnsarah@gmail.com>
2018-03-17 11:22:24 -0600

    added js link to firebase and created login.js

---
+++ assets/script/login.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB_sWSBse_GOfDx63Jh5wEj_GNs4Fyl5pI",
+    authDomain: "zekes-dice-rolling-app.firebaseapp.com",
+    databaseURL: "https://zekes-dice-rolling-app.firebaseio.com",
+    projectId: "zekes-dice-rolling-app",
+    storageBucket: "zekes-dice-rolling-app.appspot.com",
+    messagingSenderId: "735930562541"
+  };
+  firebase.initializeApp(config);
