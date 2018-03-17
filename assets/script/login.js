392ef68578e3b4caf7c977a4eeeafad1f151ddd5
Eric M <ericnsarah@gmail.com>
2018-03-17 13:41:54 -0600

    hide welcome info and display registration info on registration click

--- assets/script/login.js
+++ assets/script/login.js
@@ -1,5 +1,7 @@
 // Initialize Firebase
-var config = {
+$(document).ready(function () {
+
+  var config = {
     apiKey: "AIzaSyB_sWSBse_GOfDx63Jh5wEj_GNs4Fyl5pI",
     authDomain: "zekes-dice-rolling-app.firebaseapp.com",
     databaseURL: "https://zekes-dice-rolling-app.firebaseio.com",
@@ -8,3 +10,9 @@ var config = {
     messagingSenderId: "735930562541"
   };
   firebase.initializeApp(config);
+
+  $("#register").click(function () {
+    $("#welcomeToAdventure").addClass("hide");
+    $("#registerNewPlayer").removeClass("hide");
+  });
+});
