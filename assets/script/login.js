46ecf7bc0daf9e8f14bee45d7e1bd945bccb7604
Kailey Tetrick <kaileytetrick88@gmail.com>
2018-03-27 17:58:37 -0600

    facebook and twitter login

--- assets/script/login.js
+++ assets/script/login.js
@@ -8,3 +8,48 @@ var config = {
     messagingSenderId: "735930562541"
   };
   firebase.initializeApp(config);
+
+ //when the user clicks the login with facebook button, the popup appears to sign in with facebook. 
+ $(function() {
+   $("#facebook").click(function() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+});
+ });
+  //when the user clicks the login with twitter button, the popup appears to sign in with twitter. 
+$(function() {
+   $("#twitter").click(function() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+});
+ });
