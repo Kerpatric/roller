ca2f944b638e75af9cc5f14ef553000e981d2267
stuartmurry <stuartmurry93@gmail.com>
2018-03-30 14:32:10 -0600

    fix login and clean up

--- assets/script/player.js
+++ assets/script/player.js
@@ -1,87 +1,106 @@
-$(document).ready(function () {
-    var queue = [];
-    var sum = 0;
-    //Global Variables and Objects..
-    var dice = {
-        d2: {
-            name: 'd2',
-            max: 2,
-        },
-        d3: {
-            name: 'd3',
-            max: 3
-        },
-        d4: {
-            name: 'd4',
-            max: 4,
-        },
-        d6: {
-            name: 'd6',
-            max: 6,
-        },
-        d8: {
-            name: 'd8',
-            max: 8,
-        },
-        d10: {
-            name: 'd10',
-            max: 10
-        },
-        d12: {
-            name: 'd12',
-            max: 12,
-        },
-        d20: {
-            name: 'd20',
-            max: 20,
-        },
-        d100: {
-            name: 'd100',
-            max: 100
-        },
+$(document).ready(function() {
+
+  // Firebase
+  var config = {
+    apiKey: "AIzaSyB_sWSBse_GOfDx63Jh5wEj_GNs4Fyl5pI",
+    authDomain: "zekes-dice-rolling-app.firebaseapp.com",
+    databaseURL: "https://zekes-dice-rolling-app.firebaseio.com",
+    projectId: "zekes-dice-rolling-app",
+    storageBucket: "zekes-dice-rolling-app.appspot.com",
+    messagingSenderId: "735930562541"
+  };
+  firebase.initializeApp(config);
+
+  // Route user to login screen if they are not logged in.
+  firebase.auth().onAuthStateChanged(function(user) {
+    console.log(user);
+    if (!user) {
+      window.location = "login.html";
     }
+  });
 
-    //Dice rolling functions
-    function rolldice(max) {
-        // console.log('max');
-        // console.log(max);
-        var a = Math.floor(Math.random() * max) + 1;  // returns a number for the die roll.
-        // console.log(a);
-        return a;
-
+  var queue = [];
+  var sum = 0;
+  //Global Variables and Objects..
+  var dice = {
+    d2: {
+      name: "d2",
+      max: 2
+    },
+    d3: {
+      name: "d3",
+      max: 3
+    },
+    d4: {
+      name: "d4",
+      max: 4
+    },
+    d6: {
+      name: "d6",
+      max: 6
+    },
+    d8: {
+      name: "d8",
+      max: 8
+    },
+    d10: {
+      name: "d10",
+      max: 10
+    },
+    d12: {
+      name: "d12",
+      max: 12
+    },
+    d20: {
+      name: "d20",
+      max: 20
+    },
+    d100: {
+      name: "d100",
+      max: 100
     }
-    //When dice are clicked, they are added into the selected dice div to await roll funtion.
-    $('#dice').on('click', 'img', function () {
-        var myElement = $(this);
-        var img = myElement.clone();
-        $('#selectedDice').append(img);
-        var diceValue = $(this).attr('diceValue');
-        var ericVal =  rolldice(diceValue);
-        console.log(ericVal);
-        queue.push(ericVal);
-        console.log(queue);
-        console.log('sum');
-        sum = queue.reduce((accumulator, currentValue) => accumulator + currentValue);
-        console.log(sum);
-    
-    });
-    //Dice can be removed from the rolling que by clicking on them while in the selected dice div.
-    $('#selectedDice').on('click', 'img', function () {
-        var myElement = $(this);
-        var img = myElement.remove();
-    });
-    //When the roll dice image is clicked, all dice in the selected dice div (rolling que) are rolled
-    $('#rollDice').on('click', 'img', function () {
-        rolldice();
-        
+  };
 
-        console.log(queue);
-        //the images of the dice in the rolling que are removed
-        $("#selectedDice").html("");
-        //the sum of the roll is displayed in the results div 
-        $("#displayResults").append("Total rolled: " + sum + "<br>");
-        queue=[];
+  //Dice rolling functions
+  function rolldice(max) {
+    // console.log('max');
+    // console.log(max);
+    var a = Math.floor(Math.random() * max) + 1; // returns a number for the die roll.
+    // console.log(a);
+    return a;
+  }
+  //When dice are clicked, they are added into the selected dice div to await roll funtion.
+  $("#dice").on("click", "img", function() {
+    var myElement = $(this);
+    var img = myElement.clone();
+    $("#selectedDice").append(img);
+    var diceValue = $(this).attr("diceValue");
+    var ericVal = rolldice(diceValue);
+    console.log(ericVal);
+    queue.push(ericVal);
+    console.log(queue);
+    console.log("sum");
+    sum = queue.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    console.log(sum);
+  });
+  //Dice can be removed from the rolling que by clicking on them while in the selected dice div.
+  $("#selectedDice").on("click", "img", function() {
+    var myElement = $(this);
+    var img = myElement.remove();
+  });
+  //When the roll dice image is clicked, all dice in the selected dice div (rolling que) are rolled
+  $("#rollDice").on("click", "img", function() {
+    rolldice();
 
+    console.log(queue);
+    //the images of the dice in the rolling que are removed
+    $("#selectedDice").html("");
+    //the sum of the roll is displayed in the results div
+    $("#displayResults").append("Total rolled: " + sum + "<br>");
+    queue = [];
+  });
 
-    });
+  
 });
